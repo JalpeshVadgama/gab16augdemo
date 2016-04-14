8f95d3d3057da8551a0bb095e791f5f787ee86d4
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2016-04-15 02:08:11 +0530

    Added Employee and document repository into the code.

---
+++ GabDemoApp/GabDemoApp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace GabDemoApp.Models
+{
+    public class Employee
+    {
+        [JsonProperty(PropertyName = "employeeId")]
+        public int EmployeeId { get; set; }
+
+        [JsonProperty(PropertyName = "firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty(PropertyName = "lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty(PropertyName = "designation")]
+        public string Designation { get; set; }
+    }
+}
---
+++ GabDemoApp/GabDemoApp/Repository/DocumentDbRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace GabDemoApp.Repository
+{
+    public class DocumentDbRepository<T> where T : class
+    {
+        private readonly string _databaseId = ConfigurationManager.AppSettings["database"];
+        private readonly string _collectionId = ConfigurationManager.AppSettings["collection"];
+        private static DocumentClient _client;
+
+        public  void Initialize()
+        {
+            _client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["endpoint"]), ConfigurationManager.AppSettings["authKey"]);
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync().Wait();
+        }
+
+        private  async Task CreateDatabaseIfNotExistsAsync()
+        {
+            try
+            {
+                await _client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_databaseId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await _client.CreateDatabaseAsync(new Database { Id = _databaseId });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private  async Task CreateCollectionIfNotExistsAsync()
+        {
+            try
+            {
+                await _client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await _client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(_databaseId),
+                        new DocumentCollection { Id = _collectionId },
+                        new RequestOptions { OfferThroughput = 1000 });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
